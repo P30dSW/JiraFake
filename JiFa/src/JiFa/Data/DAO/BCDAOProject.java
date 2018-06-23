8fd0f3d7f59c82063ddd6c48d20fa9cbb7ae719b
P30dSW <pedro@gmail.com>
2018-06-24 00:08:50 +0200

    Completed ser Project UnitTest and methods Data Layer

--- JiFa/src/JiFa/Data/DAO/BCDAOProject.java
+++ JiFa/src/JiFa/Data/DAO/BCDAOProject.java
@@ -115,26 +115,146 @@ public class BCDAOProject implements DAOProject {
 
 	@Override
 	public String[] getProject(int id) {
-		// TODO Auto-generated method stub
-		return null;
+		ArrayList<String[]> projList = null;
+		String[] result = null ;
+		File fil = new File("resources/project.ser");
+		if( fil.length() == 0){
+			String[] dummy = null;
+			return dummy;
+			
+		}else{
+			
+		
+		
+		try {
+	         FileInputStream fileIn = new FileInputStream("resources/project.ser");
+	         ObjectInputStream in = new ObjectInputStream(fileIn);
+	         projList = (ArrayList<String[]>) in.readObject();
+	         in.close();
+	         fileIn.close();
+	      } catch (IOException i) {
+	         i.printStackTrace();
+	         
+	      } catch (ClassNotFoundException c) {
+	         c.printStackTrace();
+	         
+	      }
+	      
+	      for(String[] stg : projList){
+	    	  if(stg[0].equals(Integer.toString(id))){
+	    		  result = stg;
+	    		  break;
+	    	  }
+	      }
+	      return result;
+	      }
 	}
 
 	@Override
 	public ArrayList<String[]> getProjects() {
-		// TODO Auto-generated method stub
-		return null;
+		ArrayList<String[]> projList = null;
+		File fil = new File("resources/project.ser");
+		if( fil.length() == 0){
+			ArrayList<String[]> dummy = null;
+			return dummy;
+			
+		}else{
+			
+		}
+	      try {
+	         FileInputStream fileIn = new FileInputStream("resources/project.ser");
+	         ObjectInputStream in = new ObjectInputStream(fileIn);
+	         projList = (ArrayList<String[]>) in.readObject();
+	         in.close();
+	         fileIn.close();
+	      } catch (IOException i) {
+	         i.printStackTrace();
+	         
+	      } catch (ClassNotFoundException c) {
+	         c.printStackTrace();
+	         
+	      }
+	      
+		return projList;
 	}
 
 	@Override
 	public void deleteProject(int id) {
-		// TODO Auto-generated method stub
+		ArrayList<String[]> projList = null;
+		
+	      try {
+	         FileInputStream fileIn = new FileInputStream("resources/project.ser");
+	         ObjectInputStream in = new ObjectInputStream(fileIn);
+	         projList = (ArrayList<String[]>) in.readObject();
+	         in.close();
+	         fileIn.close();
+	      } catch (IOException i) {
+	         i.printStackTrace();
+	         return;
+	      } catch (ClassNotFoundException c) {
+	         c.printStackTrace();
+	         return;
+	      }
+	      
+	      for(String[] stg : projList){
+	    	  if(stg[0].equals(Integer.toString(id))){
+	    		  projList.remove(stg);
+	    		  
+	    		  break;
+	    	  }
+	      }
+	    	  projList.size();
+	    	  //clears file 
+	    	  try {
+	 	        File fil = new File("resources/project.ser");
+	 	       if(fil.exists()){
+	 	    	  fil.delete(); 
+	 	       }
+	 	       fil.createNewFile();
+	 	       
+	 	      } catch (IOException i) {
+	 	         i.printStackTrace();
+	 	         return;
+	 	      }
+	    	  
+	    	  //adds to file
+	    	  try {
+	 	         FileOutputStream fileOut = new FileOutputStream("resources/project.ser");
+	 	         ObjectOutputStream out = new ObjectOutputStream(fileOut);
+	 	         out.writeObject(projList);
+	 	         out.close();
+	 	         fileOut.close();
+	 	      } catch (IOException i) {
+	 	         i.printStackTrace();
+	 	      }
 		
 	}
 
 	@Override
 	public int getHightestUnasginedId() {
-		// TODO Auto-generated method stub
-		return 0;
+		ArrayList<String[]> projList = null;
+		
+	      try {
+	         FileInputStream fileIn = new FileInputStream("resources/project.ser");
+	         ObjectInputStream in = new ObjectInputStream(fileIn);
+	         projList = (ArrayList<String[]>) in.readObject();
+	         in.close();
+	         fileIn.close();
+	      } catch (IOException i) {
+	         i.printStackTrace();
+	      } catch (ClassNotFoundException c) {
+	         c.printStackTrace();
+	      }
+	      int highestNum = 0;
+	      for(String[] stg : projList){
+	    	  if(Integer.parseInt(stg[0]) > highestNum){
+	    		  highestNum = Integer.parseInt(stg[0]);
+	    	  }
+	      }
+	    	  
+		return highestNum + 1;
 	}
+	
+	
 
 }
--- JiFa/src/JiFa/Data/UnitTest/DAOUnitTest.java
+++ JiFa/src/JiFa/Data/UnitTest/DAOUnitTest.java
@@ -2,12 +2,15 @@ package JiFa.Data.UnitTest;
 
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+
 import org.junit.Before;
 import org.junit.Test;
 
 import JiFa.Data.Interface.DAOProject;
 import JiFa.Data.Interface.DAOTask;
 import JiFa.Data.Interface.DataFactory;
+import org.junit.Assert;
 
 public class DAOUnitTest {
 
@@ -33,8 +36,32 @@ public class DAOUnitTest {
 		//make sure the .ser is empty
 		String[] proj01 = {"2","20.06.2018","Pedro","Test Description","First Project", "30.06.2018"};
 		projDAO.createProject(proj01);
-		String[] proj02 = {"2","20.06.2018","Pedro","EDITED","First Project", "30.06.2018"};
-		projDAO.editProject(proj02, Integer.parseInt(proj02[0]));
+		String[] proj02 = {"3","20.06.2018","TEST","TEST","TEST", "30.06.2018"};
+		projDAO.createProject(proj02);
+		String[] proj03 = {"2","20.06.2018","Pedro","EDITED","First Project", "30.06.2018"};
+		projDAO.editProject(proj03, Integer.parseInt(proj03[0]));
+		ArrayList<String[]> projList = projDAO.getProjects();
+		Assert.assertEquals(2,projList.size());
+	}
+	
+	@Test
+	public void deleteProject() {
+		//make sure the .ser is empty
+		String[] proj01 = {"2","20.06.2018","Pedro","Test Description","First Project", "30.06.2018"};
+		projDAO.createProject(proj01);
+		projDAO.deleteProject( Integer.parseInt(proj01[0]));
+		ArrayList<String[]> projList = projDAO.getProjects();
+		Assert.assertEquals(0,projList.size());
+	}
+	
+	@Test
+	public void getNextProjectId() {
+		//make sure the .ser is empty
+		String[] proj01 = {"2","20.06.2018","Pedro","Test Description","First Project", "30.06.2018"};
+		projDAO.createProject(proj01);
+		String[] proj02 = {"3","20.06.2018","TEST","TEST","TEST", "30.06.2018"};
+		projDAO.createProject(proj02);
+		Assert.assertEquals(4,projDAO.getHightestUnasginedId());
 	}
 
 }
