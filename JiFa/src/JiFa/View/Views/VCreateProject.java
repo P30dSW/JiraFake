8d9653336b992ec4827d38e9dd5491ee65df07d0
Dario <Dario@192.168.0.94>
2018-06-26 22:03:41 +0200

    Logic createProject & task

--- JiFa/src/JiFa/View/Views/VCreateProject.java
+++ JiFa/src/JiFa/View/Views/VCreateProject.java
@@ -5,27 +5,33 @@ import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JTextField;
 
-public class VCreateProject extends JFrame implements ActionListener{
-	
+import JiFa.View.LogicClient.ViewLogicClient;
+
+public class VCreateProject extends JFrame implements ActionListener {
+
 	private JButton cancel, finish;
-	private JTextField name, description, date, creator
-	private JLabel name, owner, description, deadline;
-	
+	private JTextField nametxt, descriptiontxt, deadlinetxt, creatortxt, importancetxt, asigneetxt;
+	private JLabel namelbl, descriptionlbl, deadlinelbl, creatorlbl, importancelbl, asigneelbl;
+
 	public void actionPerformed(ActionEvent ae) {
 		if (ae.getSource() == finish) {
-			
+			String[] list = { "1", deadlinetxt.getText(), creatortxt.getText(), descriptiontxt.getText(),
+					nametxt.getText(), importancetxt.getText(), asigneetxt.getText() };
+			ViewLogicClient vlc = new ViewLogicClient();
+			vlc.createProject(list);
+			this.setVisible(false);
 		}
-		
+
 		if (ae.getSource() == cancel) {
-			
+			this.setVisible(false);
 		}
-		
+
 	}
-	
+
 	public VCreateProject() {
-		
+
 	}
 }
-
