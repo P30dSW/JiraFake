7726f57eddec8149323ae0fff5aeec6cbe65c0f2
P30dSW <pedro@gmail.com>
2018-06-26 21:21:06 +0200

    All body of the viewclient methodes are created

--- JiFa/src/JiFa/View/LogicClient/ViewLogicClient.java
+++ JiFa/src/JiFa/View/LogicClient/ViewLogicClient.java
@@ -5,6 +5,49 @@
  */
 package JiFa.View.LogicClient;
 
-public class ViewLogicClient {
+import java.util.ArrayList;
+
+import JiFa.Logic.Interface.LogicFactory;
+import JiFa.Logic.Interface.LogicManagement;
+import JiFa.Logic.Interface.Project;
+import JiFa.Logic.Interface.Task;
+
+public class ViewLogicClient {	
+	private LogicManagement logic;
+	
+	public ViewLogicClient() {
+		logic = LogicFactory.getLogicInstance();
+	}
+	
+	public void createProject(String[] project){
+		
+	}
+	public void editProject(String[] project){
+		
+	}
+	public void deleteProject(int projectId){
+		
+	}
+	public void createTask(String[] task,int projectId){
+		
+	}
+	public void editTask(String[] task){
+		
+	}
+	public void deleteTask(int taskId){
+		
+	}
+	public Project getProject(int projectId){
+		return null;
+	}
+	public Task getTask(int taskId){
+		return null;
+	}
+	public ArrayList<Project> getProject(){
+		return null;
+	}
+	public ArrayList<Task> getTask(){
+		return null;
+	}
 
 }
