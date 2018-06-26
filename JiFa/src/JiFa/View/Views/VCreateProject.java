ad6b9985e38e8d47c50ab163ffe3823d301de606
Dario <Dario@192.168.0.94>
2018-06-26 21:28:05 +0200

    begin vcreateproject

--- JiFa/src/JiFa/View/Views/VCreateProject.java
+++ JiFa/src/JiFa/View/Views/VCreateProject.java
@@ -3,13 +3,24 @@ package JiFa.View.Views;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JTextField;
 
 public class VCreateProject extends JFrame implements ActionListener{
 	
+	private JButton cancel, finish;
+	private JTextField name, description, date, creator
+	private JLabel name, owner, description, deadline;
+	
 	public void actionPerformed(ActionEvent ae) {
+		if (ae.getSource() == finish) {
+			
+		}
 		
-		
+		if (ae.getSource() == cancel) {
+			
+		}
 		
 	}
 	
