35ac3bdfbe6f075b72aa573a2d9dd9a6cd0148a6
lilithsz <lilith.szeszak@gibmit.ch>
2018-06-26 23:53:57 +0200

    VProject and VTask added useless things

--- JiFa/src/JiFa/View/Views/VProject.java
+++ JiFa/src/JiFa/View/Views/VProject.java
@@ -1,126 +1,95 @@
 package JiFa.View.Views;
 
-import java.awt.BorderLayout;
-import java.awt.Font;
-import java.awt.GridLayout;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.util.Date;
-
 import javax.swing.*;
-
-import JiFa.Logic.Management.AbstractMedium;
 import JiFa.View.LogicClient.ViewLogicClient;
-import com.sun.glass.events.WindowEvent;
-
 import javax.swing.JFrame;
 
 public class VProject extends JFrame implements ActionListener{
 
-    private String nameProject;
-    private String deadlineProject;
-    private String descriptionProject;
+    private String nameProject = "";
+    private String deadlineProject = "";
+    private String descriptionProject = "";
+    private String asigneeProject = "";
 
 
-        JLabel titleLabel;
-        JLabel projectNameLabel;
-        JLabel projectAsigneeLabel;
-        JLabel projectDescriptionLabel;
-        JLabel projectDeadlineLabel;
-        JLabel projectCreatorLabel;
+
+        JLabel titleLabel, projectNameLabel,  projectAsigneeLabel, projectDescriptionLabel,  projectDeadlineLabel, projectCreatorLabel;
 
         JTable taskTable;
+
+        JButton createTaskButton;
 
 
         public VProject() {
 
-            private project = vlc.getProject();
+            ViewLogicClient vlc = new ViewLogicClient();
+            setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            this.setSize(2000, 1500);
+            this.setVisible(true);
+            this.setTitle("FakeJira");
+            this.setLayout(null);
+            this.setResizable(false);
 
             titleLabel = createLabel("FakeJira", true);
+            titleLabel.setBounds(5, 5, 5, 5);
 
-            projectNameLabel = createLabel("Name", true);
 
-            projectAsigneeLabel = createLabel("Project", true);
+            projectNameLabel = createLabel(nameProject, true);
+            projectNameLabel.setBounds(5, 5, 5, 5);
+            projectNameLabel.setText("Owner: ");
+            this.add(projectName);
+
+
+            projectAsigneeLabel = createLabel(asigneeProject, true);
+            projectAsigneeLabel.setBounds(5, 5, 5, 5);
+            projectAsigneeLabel.setText("Owner: ");
+            this.add(projectAssignee);
 
             projectDescriptionLabel = createLabel("Project", true);
+            projectDescriptionLabel.setBounds(5, 5, 5, 5);
+            projectDescriptionLabel.setText("Owner: ");
+            this.add(projectDescription);
 
             projectDeadlineLabel = createLabel("Project", true);
+            projectNameLabel.setBounds(5, 5, 5, 5);
+            projectNameLabel.setText("Owner: ");
+            this.add(projectName);
 
             projectCreatorLabel = createLabel("Project", true);
+            projectCreatorLabel.setBounds(5, 5, 5, 5);
+            projectCreatorLabel.setText("Owner: ");
+            this.add(projectName);
 
 
             taskTable = new JTable();
+            JScrollPane scrollpane = new JScrollPane(taskTable);
+
 
             createTaskButton = new JButton("New Task!!");
             createTaskButton.addActionListener(this);
 
-
             setLayout(new BorderLayout());
 
-            titlePanel.setLayout(new BorderLayout());
-            numberPeoplePanel.setLayout(new GridLayout(2, 1));
-            radioPanel.setLayout(new GridLayout(2, 1));
-            radioButtonPanel.setLayout(new GridLayout(3, 1));
-            weiterPanel.setLayout(new BorderLayout());
-            everythingPanel.setLayout(new BorderLayout());
 
-            choicePanel.add(numberPeoplePanel, BorderLayout.NORTH);
-            choicePanel.add(radioPanel, BorderLayout.SOUTH);
-
-            titlePanel.add(titleLabel, BorderLayout.CENTER);
-            numberPeoplePanel.add(numberPeopleLabel, BorderLayout.CENTER);
-            numberPeoplePanel.add(numberPeopleField, BorderLayout.EAST);
-            radioPanel.add(radioLabel);
-            radioPanel.add(radioButtonPanel);
-            radioButtonPanel.add(radioButton1);
-            radioButtonPanel.add(radioButton2);
-            radioButtonPanel.add(radioButton3);
-            weiterPanel.add(weiterButton);
-
-            add(everythingPanel, BorderLayout.CENTER);
-
-            setSize(300,550);
-            setTitle("Foodinator");
-            setDefaultCloseOperation(EXIT_ON_CLOSE);
-            setVisible(true);
-        }
-
-        public static void main(String[] args) {
-            Start s = new Start();
         }
 
         private static JLabel createLabel(String caption, boolean isTitle) {
-            JLabel label = new JLabel(caption);
-            if(isTitle) {
-                label.setFont(new Font("Garamond", Font.ITALIC+Font.BOLD, 16));
-            }
-            return label;
+        JLabel label = new JLabel(caption);
+        if(isTitle) {
+            label.setFont(new Font("Garamond", Font.ITALIC+Font.BOLD, 16));
         }
-
+        return label;
+    }
 
         @Override
         public void actionPerformed(ActionEvent e) {
             // TODO Auto-generated method stub
-            if (e.getSource() == weiterButton){
-                Food f = new Food(foodTime, numberPeopleField.getText());
-                System.out.println(f.getPeopleNum());
-                Ingredients ingredients = new Ingredients(f, f.getPeopleNum()-1);
-                numberPeopleField.setText("");
-                dispose();
-            } else if (e.getSource() == radioButton1) {
-                radioButton2.setEnabled(false);
-                radioButton3.setEnabled(false);
-                this.foodTime = "Breakfast";
-
-            } else if (e.getSource() == radioButton2) {
-                radioButton1.setEnabled(false);
-                radioButton3.setEnabled(false);
-                this.foodTime = "Lunch";
-
-            } else if (e.getSource() == radioButton3) {
-                radioButton1.setEnabled(false);
-                radioButton2.setEnabled(false);
-                this.foodTime = "Dinner";
+            if (e.getSource() == createTaskButton){
+                VCreateTask ntask = new VCreateTask(xx);
+                 dispose();
             }
 
         }
--- JiFa/src/JiFa/View/Views/VTask.java
+++ JiFa/src/JiFa/View/Views/VTask.java
@@ -1,4 +1,97 @@
 package JiFa.View.Views;
 
-public class VTask {
+import JiFa.View.LogicClient.ViewLogicClient;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+
+public class VTask extends JFrame implements ActionListener {
+
+    private String nameProject = "";
+    private String deadlineProject = "";
+    private String descriptionProject = "";
+    private String asigneeProject = "";
+
+
+    JLabel titleLabel, projectNameLabel, projectAsigneeLabel, projectDescriptionLabel, projectDeadlineLabel, projectCreatorLabel;
+
+    JTable taskTable;
+
+    JButton createTaskButton;
+
+
+    public VProject() {
+
+        ViewLogicClient vlc = new ViewLogicClient();
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setSize(2000, 1500);
+        this.setVisible(true);
+        this.setTitle("FakeJira");
+        this.setLayout(null);
+        this.setResizable(false);
+
+        titleLabel = createLabel("FakeJira", true);
+        titleLabel.setBounds(5, 5, 5, 5);
+
+
+        projectNameLabel = createLabel(nameProject, true);
+        projectNameLabel.setBounds(5, 5, 5, 5);
+        projectNameLabel.setText("Owner: ");
+        this.add(projectName);
+
+
+        projectAsigneeLabel = createLabel(asigneeProject, true);
+        projectAsigneeLabel.setBounds(5, 5, 5, 5);
+        projectAsigneeLabel.setText("Owner: ");
+        this.add(projectAssignee);
+
+        projectDescriptionLabel = createLabel("Project", true);
+        projectDescriptionLabel.setBounds(5, 5, 5, 5);
+        projectDescriptionLabel.setText("Owner: ");
+        this.add(projectDescription);
+
+        projectDeadlineLabel = createLabel("Project", true);
+        projectNameLabel.setBounds(5, 5, 5, 5);
+        projectNameLabel.setText("Owner: ");
+        this.add(projectName);
+
+        projectCreatorLabel = createLabel("Project", true);
+        projectCreatorLabel.setBounds(5, 5, 5, 5);
+        projectCreatorLabel.setText("Owner: ");
+        this.add(projectName);
+
+
+        taskTable = new JTable();
+        JScrollPane scrollpane = new JScrollPane(taskTable);
+
+
+        createTaskButton = new JButton("New Task!!");
+        createTaskButton.addActionListener(this);
+
+        setLayout(new BorderLayout());
+
+
+    }
+
+    private static JLabel createLabel(String caption, boolean isTitle) {
+        JLabel label = new JLabel(caption);
+        if (isTitle) {
+            label.setFont(new Font("Garamond", Font.ITALIC + Font.BOLD, 16));
+        }
+        return label;
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        // TODO Auto-generated method stub
+        if (e.getSource() == createTaskButton) {
+            VCreateTask ntask = new VCreateTask(xx);
+            dispose();
+        }
+
+    }
+
 }
