a1a38b443bc5bd3e3a29be74b5c6e69a22cf84c1
Dario <Dario@192.168.0.94>
2018-06-26 20:58:46 +0200

    new classes and begin vcreateproject

--- JiFa/src/JiFa/View/Views/VCreate.java
+++
@@ -1,9 +0,0 @@
-package JiFa.View.Views;
-
-public class VCreate {
-	
-	public VCreate() {
-		System.out.println("fisch");
-	}
-}
-
---
+++ JiFa/src/JiFa/View/Views/VCreateProject.java
@@ -0,0 +1,20 @@
+package JiFa.View.Views;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JFrame;
+
+public class VCreateProject extends JFrame implements ActionListener{
+	
+	public void actionPerformed(ActionEvent ae) {
+		
+		
+		
+	}
+	
+	public VCreateProject() {
+		
+	}
+}
+
