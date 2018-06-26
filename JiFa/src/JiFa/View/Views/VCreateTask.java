1b3f43ab38e68131f32a940a6951442b8cd073ae
Dario <Dario@192.168.0.94>
2018-06-26 23:01:34 +0200

    createproject & createtask

--- JiFa/src/JiFa/View/Views/VCreateTask.java
+++ JiFa/src/JiFa/View/Views/VCreateTask.java
@@ -34,5 +34,74 @@ public class VCreateTask extends JFrame implements ActionListener {
 
 	public VCreateTask(int projectID) {
 		this.projectID = projectID;
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setSize(1000, 700);
+		this.setVisible(true);
+		this.setTitle("New Task");
+		this.setLayout(null);
+		this.setResizable(false);
+		
+		namelbl = new JLabel();
+		namelbl.setBounds(5, 5, 5, 5);
+		namelbl.setText("Owner: ");
+		this.add(namelbl);
+		
+		nametxt = new JTextField();
+		nametxt.setBounds(10, 10, 10, 10);
+		this.add(nametxt);
+		
+		
+		descriptionlbl = new JLabel();
+		descriptionlbl.setBounds(5, 5, 5, 5);
+		descriptionlbl.setText("Description: ");
+		this.add(descriptionlbl);
+		
+		descriptiontxt = new JTextField();
+		descriptiontxt.setBounds(10, 10, 10, 10);
+		this.add(descriptiontxt);
+		
+		
+		deadlinelbl = new JLabel();
+		deadlinelbl.setBounds(5, 5, 5, 5);
+		deadlinelbl.setText("Deadline: ");
+		this.add(deadlinelbl);
+		
+		deadlinetxt = new JTextField();
+		deadlinetxt.setBounds(10, 10, 10, 10);
+		this.add(deadlinetxt);
+		
+		
+		creatorlbl = new JLabel();
+		creatorlbl.setBounds(5, 5, 5, 5);
+		creatorlbl.setText("Creator: ");
+		this.add(creatorlbl);
+		
+		creatortxt = new JTextField();
+		creatortxt.setBounds(10, 10, 10, 10);
+		this.add(creatortxt);
+		
+		
+		importancelbl = new JLabel();
+		importancelbl.setBounds(5, 5, 5, 5);
+		importancelbl.setText("Importance: ");
+		this.add(importancelbl);
+		
+		importancetxt = new JTextField();
+		importancetxt.setBounds(10, 10, 10, 10);
+		this.add(importancetxt);
+		
+		
+		asigneelbl = new JLabel();
+		asigneelbl.setBounds(5, 5, 5, 5);
+		asigneelbl.setText("Asignee: ");
+		this.add(asigneelbl);
+		
+		asigneetxt = new JTextField();
+		asigneetxt.setBounds(10, 10, 10, 10);
+		this.add(asigneetxt);
+		
+		
+		
+		
 	}
 }
